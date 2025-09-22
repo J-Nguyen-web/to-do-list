957b9ad4f35f1b08a4e8509269d625de06af71bb
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-09-22 22:33:08 +0300

    script structure
    main logic structure

---
+++ script.js
@@ -0,0 +1,14 @@
+const taskInput = document.getElementById('taskInput')
+const taskList = document.getElementById('taskList')
+
+function newTask(){
+
+}
+
+function renderTasks() {
+
+}
+
+function saveTask() {
+    
+}
