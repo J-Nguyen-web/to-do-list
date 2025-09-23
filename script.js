8ca1f8bb169a228eb4bb3a406ba96e03eec496d5
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-09-23 12:31:37 +0300

    functionality added
    new task
    render tasks

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
 const taskInput = document.getElementById('taskInput')
 const taskList = document.getElementById('taskList')
 
-function newTask(){
-
+window.onload = () => {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    savedTasks.forEach(task => renderTasks(task.text, task.done))
 }
 
-function renderTasks() {
+function newTask(){
+    const taskText = taskInput.value.trim();
+    if(taskText === '') {
+        alert('Please enter a task!')
+        return;
+    }
+    renderTasks(taskText, false);
+    saveTask();
+    taskInput.value = '';
+}
 
+function renderTasks(text, done) {
+    const li = document.createElement('li');    
+
+    const span = document.createElement('span');
+    span.textContent = text;
+    if(done) {
+        li.classList.add('done')
+    }
+
+    span.onclick = () => {
+        li.classList.toggle
+        saveTask();
+    }
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
 }
 
 function saveTask() {
-    
+    const tasks = [];
+    taskList.querySelectorAll('li').forEach( li => {
+        tasks.push({
+        text: li.querySelector('span').textContent,
+        done: li.classList.contains('done')
+        });
+    });
+    localStorage.setItem('tasks',JSON.stringify(tasks))
 }
+
+taskInput.addEventListener('keypress',function(event){
+    if(event.key === 'Enter'){
+        newTask();
+    }
+});
