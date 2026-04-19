105f84a4d9c1191a9b844421e258a38ac10e0b1a
J-Nguyen-web <map.genshin.impacts@gmail.com>
2026-04-19 11:47:35 +0300

    add helper for mouse drag

--- script.js
+++ script.js
@@ -321,3 +321,19 @@ function enableDragDrop(li) {
         }
     })
 }
+
+// === Helper for mouse drag === //
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('li:not(.dragging')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+
+        if(offset< 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+        } else {
+            return closest
+        }
+        }, { offset: Number.NEGATIVE_INFINITY}).element
+}
